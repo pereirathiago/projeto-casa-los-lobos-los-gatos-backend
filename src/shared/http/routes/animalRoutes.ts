a6554b993d38ed14bd62f2561ac63f2bcb543993
Thiago Pereira <pereira.tp.thiago@gmail.com>
2025-11-22 23:01:23 -0300

    feat(animals): rotas de listagem de animais para usuarios sponsor

--- src/shared/http/routes/animalRoutes.ts
+++ src/shared/http/routes/animalRoutes.ts
@@ -16,23 +16,29 @@ const uploadAnimalImages = multer(uploadConfig)
 const animalController = container.resolve(AnimalController)
 
 router.use(ensureAuthenticated)
-router.use(ensureAdminRole)
 
 router.get('/', (req, res) => animalController.getAll(req, res))
 router.get('/:uuid', (req, res) => animalController.getById(req, res))
 
-router.post('/', uploadAnimalImages.array('arquivos', 3), validateCreateAnimal, (req, res) =>
-  animalController.create(req, res),
+router.post(
+  '/',
+  ensureAdminRole,
+  uploadAnimalImages.array('arquivos', 3),
+  validateCreateAnimal,
+  (req, res) => animalController.create(req, res),
 )
 
-router.put('/:uuid', validateUpdateAnimal, (req, res) => animalController.update(req, res))
-
-router.post('/:uuid/photos', uploadAnimalImages.single('arquivo'), (req, res) =>
+router.put('/:uuid', ensureAdminRole, validateUpdateAnimal, (req, res) =>
+  animalController.update(req, res),
+)
+router.post('/:uuid/photos', ensureAdminRole, uploadAnimalImages.single('arquivo'), (req, res) =>
   animalController.addPhoto(req, res),
 )
 
-router.delete('/:uuid/photos/:photoUuid', (req, res) => animalController.deletePhoto(req, res))
+router.delete('/:uuid/photos/:photoUuid', ensureAdminRole, (req, res) =>
+  animalController.deletePhoto(req, res),
+)
 
-router.delete('/:uuid', (req, res) => animalController.delete(req, res))
+router.delete('/:uuid', ensureAdminRole, (req, res) => animalController.delete(req, res))
 
 export default router
