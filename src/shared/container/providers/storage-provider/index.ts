47431ac6adf06637e65ca0ffa299952b59197c47
Thiago Pereira <pereira.tp.thiago@gmail.com>
2025-11-29 14:50:58 -0300

    feat(aws): conectar aws s3 para o cloudflare r2

--- src/shared/container/providers/storage-provider/index.ts
+++ src/shared/container/providers/storage-provider/index.ts
@@ -1,9 +1,11 @@
 import { container } from 'tsyringe'
 import { IStorageProvider } from './i-storage-provider.js'
 import { LocalStorageProvider } from './implementations/local-storage-provider.js'
+import { S3AwsStorageProvider } from './implementations/s3-aws-storage-provider.js'
 
 const diskStorage = {
   local: LocalStorageProvider,
+  s3Aws: S3AwsStorageProvider,
 }
 
 container.registerSingleton<IStorageProvider>(
